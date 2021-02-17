0802965ac52ef1a0f1679138c5e1743a9e6acbdd
Pavel Korol <p_korol@wargaming.net>
2021-02-17 22:07:38 +0300

    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,106 +1,44 @@
-const isMobile = 'ontouchstart' in window || navigator.msMaxTouchPoints;
-
-/* Scroll */
-const timeout = 600;
-let isAninimating = false;
-let activePage = 0;
-
-const pagesElements = document.querySelectorAll('.page');
-const navigationElement = document.querySelector('.navigation');
-
-function prevPage() {
-    if (activePage !== 0) {
-        activePage--;
-    }
-    scrollPages();
-}
-
-function nextPage() {
-    if (activePage < pagesElements.length - 1) {
-        activePage++;
-    }
-    scrollPages();
-}
-
-function scrollHandler(e, inverse = false) {
-    if (isAninimating) {
-        return;
-    }
-
-    if (e) {
-        var isScrollDown = e.deltaY >= 0;
-        if (inverse) {
-            isScrollDown = !isScrollDown;
-        }
-        if (isScrollDown) {
-            nextPage()
-        } else {
-            prevPage();
-        }
-    }
-}
-
-function scrollPages() {
-    if (isAninimating) {
-        return;
-    }
-
-    isAninimating = true;
-    setTimeout(() => {
-        isAninimating = false;
-    }, timeout);
-    
-    pagesElements.forEach(el => el.classList.remove('active'));
-    pagesElements[activePage].classList.add('active');
-    navigationElement.dataset.slide = activePage;
-}
-
-if (!isMobile) {
-    document.addEventListener('wheel', scrollHandler, { capture: false, passive: true });
-}
-
-function navigateToPage() {
-    switch (document.location.hash) {
-        case '#about':
-            activePage = 1;
-            break;
-        case '#products':
-            activePage = 2;
-            break;
-        case '#forum':
-            activePage = 3;
-            break;
-        case '#footer':
-            activePage = 4;
-            break;
-        default:
-            activePage = 0;
-    }
-    scrollPages();
-}
-
-window.addEventListener('popstate', function() {
-    navigateToPage();
-});
-
-window.scrollTo(0, 0);
-navigateToPage();
-
-var hammer = new Hammer(document.body, {});
-hammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-hammer.on('swipe', function(e) {
-    scrollHandler(e, true);
-});
-
-document.querySelector('.js-nav-prev').addEventListener('click', prevPage);
-document.querySelector('.js-nav-next').addEventListener('click', nextPage);
-
 /* Popup */
+const popupToggleElements = document.querySelectorAll('.js-popup-toggle')
 const popupElement = document.querySelector('.popup');
-const fadeElement = document.querySelector('.fade');
 
 function togglePopup() {
     popupElement.classList.toggle('opened');
 }
 
-fadeElement.addEventListener('click', togglePopup);
+popupToggleElements.forEach(el => el.addEventListener('click', (e) => {
+    e.preventDefault();
+    togglePopup();
+}))
+
+
+/* cookies */
+new CookiesEuBanner(function () {
+    // Your code here
+});
+
+/* video */
+const videoElement = document.querySelector('.phone_video');
+
+if (videoElement) {
+    const observerCallback = function (e) {
+        if (e[0].isIntersecting) {
+            videoElement.play();
+        } else {
+            videoElement.pause();
+        }
+    };
+
+    const observer = new IntersectionObserver(observerCallback, {
+        rootMargin: '0px',
+        threshold: 0.3
+    });
+    observer.observe(videoElement);
+}
+
+/* language selector */
+const langElement = document.querySelector('.lang');
+const langToggleElement = document.querySelector('.lang_item');
+langToggleElement.addEventListener('click', function(e) {
+    langElement.classList.toggle('opened');
+})
---
+++ src/js/navigation.js
@@ -0,0 +1,98 @@
+const isMobile = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+
+/* Scroll */
+const timeout = 600;
+let isAninimating = false;
+let activePage = 0;
+
+const pagesElements = document.querySelectorAll('.page');
+const pagesBgElement = document.querySelector('.pages-container_background');
+const navigationElement = document.querySelector('.navigation');
+
+function prevPage() {
+    if (activePage !== 0) {
+        activePage--;
+    }
+    scrollPages();
+}
+
+function nextPage() {
+    if (activePage < pagesElements.length - 1) {
+        activePage++;
+    }
+    scrollPages();
+}
+
+function scrollHandler(e, inverse = false) {
+    if (isAninimating) {
+        return;
+    }
+
+    if (e) {
+        var isScrollDown = e.deltaY >= 0;
+        if (inverse) {
+            isScrollDown = !isScrollDown;
+        }
+        if (isScrollDown) {
+            nextPage()
+        } else {
+            prevPage();
+        }
+    }
+}
+
+function scrollPages() {
+    if (isAninimating) {
+        return;
+    }
+
+    isAninimating = true;
+    setTimeout(() => {
+        isAninimating = false;
+    }, timeout);
+
+    pagesElements.forEach(el => el.classList.remove('active'));
+    pagesElements[activePage].classList.add('active');
+    navigationElement.dataset.slide = activePage;
+    pagesBgElement.dataset.slide = activePage;
+}
+
+if (!isMobile && pagesElements.length) {
+    document.addEventListener('wheel', scrollHandler, { capture: false, passive: true });
+}
+
+function navigateToPage() {
+    switch (document.location.hash) {
+        case '#about':
+            activePage = 1;
+            break;
+        case '#products':
+            activePage = 2;
+            break;
+        case '#forum':
+            activePage = 3;
+            break;
+        case '#footer':
+            activePage = 4;
+            break;
+        default:
+            activePage = 0;
+    }
+    scrollPages();
+}
+
+window.addEventListener('popstate', function () {
+    navigateToPage();
+});
+
+window.scrollTo(0, 0);
+navigateToPage();
+
+//var hammer = new Hammer(document.body, {});
+//hammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+//hammer.on('swipe', function (e) {
+//    scrollHandler(e, true);
+//});
+
+document.querySelector('.js-nav-prev').addEventListener('click', prevPage);
+document.querySelector('.js-nav-next').addEventListener('click', nextPage);
