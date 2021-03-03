d80fb3ffdcef6edaafb67dfb37a53e39fa3c4dcd
Pavel Korol <p_korol@wargaming.net>
2021-03-03 13:40:09 +0300

    nl

---
+++ new-layer/src/js/main.js
@@ -0,0 +1,50 @@
+/* Popup */
+const popupToggleElements = document.querySelectorAll('.js-popup-toggle')
+const popupElement = document.querySelector('.popup');
+
+function togglePopup() {
+    popupElement.classList.toggle('opened');
+}
+
+popupToggleElements.forEach(el => el.addEventListener('click', (e) => {
+    e.preventDefault();
+    togglePopup();
+}))
+
+
+/* cookies */
+const hasCookies = Cookies.get('CookieNotificationCookie');
+
+const cookiesBanner = document.querySelector('.cookies-banner');
+const cookiesAcceptButton = document.getElementById('cookies-eu-accept');
+const cookiesRejectButton = document.getElementById('cookies-eu-reject');
+cookiesAcceptButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    cookiesBanner.style.display = 'none';
+    Cookies.set('CookieNotificationCookie', 'true', { expires: 365 });
+});
+cookiesRejectButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    cookiesBanner.style.display = 'none';
+    localStorage.acceptCookies = 'false';
+});
+
+if (cookiesBanner) {
+    if (!hasCookies && !localStorage.acceptCookies) {
+        cookiesBanner.style.display = 'block';
+    }
+}
+
+/* language selector */
+const langElement = document.querySelector('.lang');
+const langToggleElement = document.querySelector('.lang_item');
+
+document.addEventListener('click', function(e) {
+    if (e.target === langElement || e.target === langToggleElement) {
+        langElement.classList.add('opened');
+    } else {
+        langElement.classList.remove('opened');
+    }
+});
---
+++ new-layer/src/js/parallax.js
@@ -0,0 +1,67 @@
+const parallaxContentElement = document.querySelector('.parallax_content');
+const parallaxBackElement = document.querySelector('.parallax_background');
+const parallaxFrontElement = document.querySelector('.parallax_front');
+
+function setBackgroundSize() {
+    const height = parallaxContentElement.getBoundingClientRect().height.toFixed(0);
+    //console.log(height, height / 3);
+    parallaxBackElement.style.height = height / 3;
+    parallaxFrontElement.style.height = height / 1.5;
+}
+
+window.addEventListener('load', setBackgroundSize);
+window.addEventListener('resize', setBackgroundSize);
+
+/* video */
+const videoElement = document.querySelector('.phone_video');
+
+if (videoElement) {
+    const observerCallback = function (e) {
+        if (e[0].isIntersecting) {
+            videoElement.play();
+        } else {
+            videoElement.pause();
+        }
+    };
+
+    const observer = new IntersectionObserver(observerCallback, {
+        rootMargin: '0px',
+        threshold: 0.3
+    });
+    observer.observe(videoElement);
+}
+
+const animatedElements = document.querySelectorAll('.js-animated');
+const thresholdSteps = [...Array(100).keys()].map(i => i / 100);
+
+if (animatedElements.length) {
+    const observerCallback = function (e) {
+        const { boundingClientRect, intersectionRatio, target } = e[0];
+        if (boundingClientRect.bottom > 500) {
+            const ratio = intersectionRatio.toFixed(2);
+
+            if (target.dataset.transform) {
+                const [transformX, transformY] = target.dataset.transform.split(',');
+                target.style.transform = `translate(${transformX - (transformX * ratio)}%, ${transformY - (transformY * ratio)}%)`;
+            }
+            if (target.dataset.scale) {
+                const scale = target.dataset.scale;
+                target.style.transform = `scale(${parseFloat(scale) + ((1 - scale) * ratio)})`;
+            }
+
+            target.style.opacity = intersectionRatio;
+        }
+    };
+
+    animatedElements.forEach(el => {
+        const observer = new IntersectionObserver(observerCallback, {
+            rootMargin: '0px 0px -25% 0px',
+            threshold: thresholdSteps,
+            root: document.querySelector('.parallax')
+        });
+        observer.observe(el);
+    })
+}
+
+
+
